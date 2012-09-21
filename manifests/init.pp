873335f6156c52d801942df2acf1e86600223711
Lee Boynton <lee@lboynton.com>
2012-09-21 16:00:10 +0100

    Add newer init script for memcache to allow multiple instances

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,9 +9,31 @@ class memcached() {
         require => Class['rpmforge'],
     }
     
+    file { "/etc/init.d/memcached":
+        ensure => present,
+        source => "puppet:///modules/memcached/memcached-init",
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        alias  => 'memcached-init',
+    }
+    
+    file { "/usr/share/memcached/scripts/start-memcached":
+        ensure => present,
+        source => "puppet:///modules/memcached/start-memcached",
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+        alias  => 'memcached-init',
+    }
+    
     service { "memcached":
         ensure   => running,
         enable   => true,
-        require  => Package['memcached']
+        require  => [
+            Package['memcached'],
+            File["memcached-init"],
+            File["memcached-bin"],
+        ]
     }
 }
