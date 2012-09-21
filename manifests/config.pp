5501813900b755fc39cc72fee774c8a4a765fe57
Lee Boynton <lee@lboynton.com>
2012-09-21 14:02:28 +0100

    Add missing .conf extension

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,5 @@
 define memcached::config(port, memory) {
-    file { "/etc/memcached_${name}":
+    file { "/etc/memcached_${name}.conf":
         content => template("memcached/memcached.conf.erb")
     }
 }
