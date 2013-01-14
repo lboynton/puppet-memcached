d1b845588dc49929f6b6beaca988f8b9175c9479
Lee Boynton <lee@lboynton.com>
2013-01-14 12:02:01 +0000

    Fix some lint warnings

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,10 +1,10 @@
 define memcached::config(
-    $port			= 11211,	# Port number to listen on
-    $memory			= 64,		# Maximum amount of memory to use
-    $listen			= false,	# The IP address to listen on, defaults to all
-	$connections	= 1024,		# Limit the number of simultaneous incoming connections
+    $port           = 11211,    # Port number to listen on
+    $memory         = 64,       # Maximum amount of memory to use
+    $listen         = false,    # The IP address to listen on, defaults to all
+    $connections    = 1024,     # Limit the number of simultaneous incoming connections
 ) {
     file { "/etc/memcached_${name}.conf":
-        content => template("memcached/memcached.conf.erb")
+        content => template('memcached/memcached.conf.erb')
     }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,26 @@
 class memcached() {
     # use memcache from rpmforge-extras repo
-    class { "rpmforge":
+    class { 'rpmforge':
         testing => 1
     }
 
-    package { "memcached":
+    package { 'memcached':
         ensure  => installed,
         require => Class['rpmforge'],
     }
-    
-    file { "/etc/init.d/memcached":
+
+    file { '/etc/init.d/memcached':
         ensure => present,
-        source => "puppet:///modules/memcached/memcached-init",
+        source => 'puppet:///modules/memcached/memcached-init',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
         alias  => 'memcached-init',
     }
-    
-    file { "/usr/bin/start-memcached":
+
+    file { '/usr/bin/start-memcached':
         ensure => present,
-        source => "puppet:///modules/memcached/start-memcached",
+        source => 'puppet:///modules/memcached/start-memcached',
         owner  => 'root',
         group  => 'root',
         mode   => '0755',
@@ -28,9 +28,9 @@ class memcached() {
     }
 
     # todo: can be removed? Do we need this? Should just be using memcached_*.conf.
-    file { "/etc/memcached.conf":
+    file { '/etc/memcached.conf':
         ensure => present,
-        source => "puppet:///modules/memcached/memcached.conf",
+        source => 'puppet:///modules/memcached/memcached.conf',
         owner  => 'root',
         group  => 'root',
         mode   => '0644',
@@ -41,16 +41,16 @@ class memcached() {
         ensure => present,
         system => true,
     }
-    
-    service { "memcached":
+
+    service { 'memcached':
         ensure   => running,
         enable   => true,
         require  => [
             Package['memcached'],
-            File["memcached-init"],
-            File["memcached-bin"],
-            File["memcached-conf"],
-            User["memcached"],
+            File['memcached-init'],
+            File['memcached-bin'],
+            File['memcached-conf'],
+            User['memcached'],
         ]
     }
 }
