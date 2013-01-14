23001c46003ca1666720065f91bc1b83eca93661
Lee Boynton <lee@lboynton.com>
2013-01-14 11:24:46 +0000

    Allow number of connections limit to be configured

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,8 @@
 define memcached::config(
     $port, 
     $memory,
-    $listen = false,
+    $listen			= false,
+	$connections	= 1024,		# Limit the number of simultaneous incoming connections
 ) {
     file { "/etc/memcached_${name}.conf":
         content => template("memcached/memcached.conf.erb")
