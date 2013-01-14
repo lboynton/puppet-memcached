0b6af4f474f4240984083f955f50437f1c33fb58
Lee Boynton <lee@lboynton.com>
2013-01-14 11:26:24 +0000

    Add documentation

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,7 @@
 define memcached::config(
-    $port			= 11211, 
-    $memory			= 64,
-    $listen			= false,
+    $port			= 11211,	# Port number to listen on
+    $memory			= 64,		# Maximum amount of memory to use
+    $listen			= false,	# The IP address to listen on, defaults to all
 	$connections	= 1024,		# Limit the number of simultaneous incoming connections
 ) {
     file { "/etc/memcached_${name}.conf":
